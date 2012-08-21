de13c408ef2122dd27fd3d079fc440b124a1ad51
AlfonsoUceda <ferrarialobestia@hotmail.com>
2012-08-21 14:05:41 +0200

    Añadido esqueleto de la aplicación.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webshop::Application.config.secret_token = 'fbb6fb0a2bba89a086a33f6d3e1b05c33ed6efc87ffb2695299ac87e309a8b1009c1e0bd08d7e02a6570c3e2625d6536962a25cb97a7c1382ea533f2236ff85d'
