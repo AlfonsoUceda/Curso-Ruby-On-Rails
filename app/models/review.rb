9aa10013f277919664c7e24bdfa28951f55b6eb7
AlfonsoUceda <ferrarialobestia@hotmail.com>
2012-08-27 12:59:13 +0200

    - Añadidas validaciones a los modelos review y user

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,14 @@
 class Review < ActiveRecord::Base
   attr_accessible :description, :rating, :summary
 
-  belongs_to :product, :counter_cache => true
+  belongs_to :product, counter_cache: true
   belongs_to :user
+
+  validates :description, presence: true,
+  					  length: { minimum: 10 }
+  validates :rating, presence: true
+  validates :summary, presence: true,
+  					  length: { minimum: 3 }
 
   def username
     user = User.find(self.user_id)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,25 @@
+class EmailValidator < ActiveModel::EachValidator
+	def validate_each(record, attribute, value)
+		unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      		record.errors[attribute] << (options[:message] || "is not an email")
+    	end
+	end
+end
+
 class User < ActiveRecord::Base
   attr_accessible :email, :firstname, :lastname, :password, :password_confirmation
 	
   has_many :reviews
 
   has_secure_password
+
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :email, presence: true,
+                    uniqueness: true,
+                    email: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
 end
+
+
